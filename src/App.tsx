5f6806012e18ec050a73ee8a1a5f0f7801efccdf
krendeleno <krendeleno@gmail.com>
2022-04-10 18:07:16 +0300

    fix: Фикс бага с роутером

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Main } from "./pages/Main";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="superchat">
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
