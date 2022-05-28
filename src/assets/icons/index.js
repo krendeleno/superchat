030fddb65203236f4923caae5b976e64d304ef67
krendeleno <krendeleno@gmail.com>
2022-05-29 01:53:00 +0300

    feat: Добавлена иконка хэштега в форме создания чата

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,6 @@
 export { default as Arrow } from "./Arrow";
 export { default as Cross } from "./Cross";
 export { default as CrossSmall } from "./CrossSmall";
+export { default as Hashtag } from "./Hashtag";
 export { default as Logout } from "./Logout";
 export { default as Plus } from "./Plus";
