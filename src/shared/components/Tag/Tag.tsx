b540fd9a0fc206c03d874e87a757120793feab2f
krendeleno <krendeleno@gmail.com>
2022-05-30 23:56:07 +0300

    fix: Убрана лишняя анимация на тегах внутри чата при драге

--- src/shared/components/Tag/Tag.tsx
+++ src/shared/components/Tag/Tag.tsx
@@ -9,41 +9,62 @@ import { CrossSmall } from "src/assets/icons";
 import styles from "src/shared/components/Tag/Tag.module.css";
 
 type TagProps = {
-  tag: string,
-  className?: string,
-  isActive?: boolean,
-  onClick?: (event: any) => void,
-  shadow?: boolean,
-  small?: boolean,
-  color?: ThemeColors,
-  deletable?: boolean,
-}
+  tag: string;
+  className?: string;
+  isActive?: boolean;
+  onClick?: (event: any) => void;
+  shadow?: boolean;
+  small?: boolean;
+  color?: ThemeColors;
+  deletable?: boolean;
+};
 
-export const Tag = observer(({ tag, className, shadow, small, isActive, color, deletable = false, ...rest }: TagProps) => {
-  const [showDelete, setShowDelete] = useState(false);
+export const Tag = observer(
+  ({
+    tag,
+    className,
+    shadow,
+    small,
+    isActive,
+    color,
+    deletable = false,
+    ...rest
+  }: TagProps) => {
+    const [showDelete, setShowDelete] = useState(false);
 
-  return (
-        <motion.div {...rest}
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.3 }}
-                    className={[
-          styles['Tag'],
+    return (
+      <motion.div
+        {...rest}
+        initial={small ? false : { opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.3 }}
+        className={[
+          styles["Tag"],
           className,
-          isActive ? styles['Tag_active'] : "",
-          shadow ? styles['Tag_shadow'] : "",
-          small ? styles['Tag_small'] : "",
-        ].filter(x => x).join(' ')}
-             style={color ? {backgroundColor: themes[color].secondary} : {}}
-             onMouseEnter={() => setShowDelete(true)}
-             onMouseLeave={() => setShowDelete(false)}
-        >
-          <AnimatePresence>
-          {showDelete && deletable && <motion.div initial={{ opacity: 0, scale: 0.5 }}
-                                                  animate={{ opacity: 1, scale: 1 }}
-                                                  exit={{ opacity: 0, scale: 0.3 }}  className={styles['Tag_delete']}><CrossSmall/></motion.div>}
-          </AnimatePresence>
-            <span className={styles['Tag_text']}>{tag}</span>
-        </motion.div>
-  )
-});
+          isActive ? styles["Tag_active"] : "",
+          shadow ? styles["Tag_shadow"] : "",
+          small ? styles["Tag_small"] : "",
+        ]
+          .filter((x) => x)
+          .join(" ")}
+        style={color ? { backgroundColor: themes[color].secondary } : {}}
+        onMouseEnter={() => setShowDelete(true)}
+        onMouseLeave={() => setShowDelete(false)}
+      >
+        <AnimatePresence>
+          {showDelete && deletable && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.3 }}
+              className={styles["Tag_delete"]}
+            >
+              <CrossSmall />
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <span className={styles["Tag_text"]}>{tag}</span>
+      </motion.div>
+    );
+  }
+);
